eab2503d6e4702f24acbccb862f17440815155dd
Rozhiar Omer <rozyar.313175000@uhd.edu.iq>
2023-06-28 03:48:18 +0300

    added currency list

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,11 +1,41 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import 'package:currency_exchanger_with_getx/app/utilities/GlobalVariebles.dart';
 
 class HomeController extends GetxController {
   //TODO: Implement HomeController
+  RxString title = 'Convert'.obs;
+  RxString selectedCurrency = 'USD'.obs;
+  RxString secondSelectedCurrency = 'USD'.obs;
+  RxList currencyList = [].obs;
+  RxString apiKey = GlobalVariebles.apiKey.obs;
+  TextEditingController amountController = TextEditingController();
+
+  // function to get all the codes for the currencies
+  Future<List<String>> getAvailableCurrencies() async {
+    final url = 'https://v6.exchangerate-api.com/v6/$apiKey/codes';
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      var data = json.decode(response.body);
+      List<String> currencies = [];
+      for (var currency in data['supported_codes']) {
+        currencyList.add(currency);
+      }
+
+      return currencies;
+    } else {
+      throw Exception('Failed to fetch currencies');
+    }
+  }
 
   final count = 0.obs;
   @override
   void onInit() {
+    getAvailableCurrencies();
     super.onInit();
   }
 
---
+++ lib/app/utilities/GlobalVariebles.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/widgets.dart';
+
+class GlobalVariebles {
+  static final GlobalKey<NavigatorState> navigatorKey =
+      GlobalKey<NavigatorState>();
+  static const String apiKey = "57748543f82d9394f3ed49cf";
+  static const String baseUrl = "https://v6.exchangerate-api.com/v6/";
+}
